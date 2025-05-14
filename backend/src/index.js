f57015bc0b675fe444e3ae064cdf888f486f88bd
Harshwagh21 <harshwagh8411@gmail.com>
2025-05-14 17:22:52 +0530

    Edited Port setting

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,13 @@ import cors from "cors";
 import clientroutes from "./routes/clientroutes.js";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/", clientroutes);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log("listening on port 3000");
 });
