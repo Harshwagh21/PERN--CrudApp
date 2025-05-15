78bfdb0decb5a12259305cf4b4c685d6e6169dad
Harshwagh21 <harshwagh8411@gmail.com>
2025-05-15 10:54:38 +0530

    AWS Deploy changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,13 @@ import cors from "cors";
 import clientroutes from "./routes/clientroutes.js";
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/", clientroutes);
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log("listening on port 3000");
 });
